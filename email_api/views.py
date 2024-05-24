79df0e9ac629722887a96a7905895462c0390071
mezeiser <mezardini@gmail.com>
2024-05-24 19:43:57 +0100

    worked on the email template

--- email_api/views.py
+++ email_api/views.py
@@ -5,6 +5,7 @@ from rest_framework.views import APIView
 from django.http import HttpResponse
 from rest_framework.response import Response
 from rest_framework import generics, status, viewsets
+from django.core.mail import EmailMessage
 
 # Create your views here.
 
@@ -68,6 +69,77 @@ class SendEmail(generics.CreateAPIView):
         return redirect('https://feedbackflow.netlify.app/')
 
 
+html_template = '''
+<body style="background-color: #f7f5fa; margin: 0 !important; padding: 0 !important;">
+    <table border="0" cellpadding="0" cellspacing="0" width="100%">
+        <tr>
+            <td bgcolor="#426899" align="center">
+                <table border="0" cellpadding="0" cellspacing="0" width="480">
+                    <tr>
+                        <td align="center" valign="top" style="padding: 40px 10px 40px 10px;">
+                            <div style="display: block; font-family: Helvetica, Arial, sans-serif; color: #ffffff; font-size: 18px;"
+                                border="0"></div>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+        <tr>
+            <td bgcolor="#426899" align="center" style="padding: 0px 10px 0px 10px;">
+                <table border="0" cellpadding="0" cellspacing="0" width="480">
+                    <tr>
+                        <td bgcolor="#ffffff" align="left" valign="top"
+                            style="padding: 30px 30px 20px 30px; border-radius: 4px 4px 0px 0px; color: #111111; font-family: Helvetica, Arial, sans-serif; font-size: 48px; font-weight: 400; line-height: 48px;">
+                            <!-- Add your header content here -->
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+        <tr>
+            <td bgcolor="#f4f4f4" align="center" style="padding: 0px 10px 0px 10px;">
+                <table border="0" cellpadding="0" cellspacing="0" width="480">
+                    <tr>
+                        <td bgcolor="#ffffff" align="left">
+                            <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                <tr>
+                                    <th align="left" valign="top"
+                                        style="padding-left:30px;padding-right:15px;padding-bottom:10px; font-family: Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
+                                        E-Mail</th>
+                                    <td align="left" valign="top"
+                                        style="padding-left:15px;padding-right:30px;padding-bottom:10px;font-family: Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
+                                        {email}</td>
+                                </tr>
+                                <tr>
+                                    <th align="left" valign="top"
+                                        style="padding-left:30px;padding-right:15px;padding-bottom:10px; font-family: Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
+                                        Name</th>
+                                    <td align="left" valign="top"
+                                        style="padding-left:15px;padding-right:30px;padding-bottom:10px;font-family: Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
+                                        {name}</td>
+                                </tr>
+                                <tr>
+                                    <td colspan="2"
+                                        style="padding-left:30px;padding-right:15px;padding-bottom:10px; font-family: Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 400; line-height: 25px;">
+                                        <p>Message:</p>
+                                        <p>{message}</p>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+</body>
+'''
+
+
+def generate_email_html(email, name, message):
+    return html_template.format(email=email, name=name, message=message)
+
+
 class SendEmailFF(generics.CreateAPIView):
     serializer_class = SendEmailSerializer
 
@@ -81,19 +153,29 @@ class SendEmailFF(generics.CreateAPIView):
         redirect_url = request.data.get('redirect_url')
 
         subject = f'Message from {name}'
-        body = f'''
-        Hello Mezard,\n
-        You have received a message from {name}.\n
-        With email: {email}\n
-        Message: {message}\n'''
+        # body = f'''
+        # Hello Mezard,\n
+        # You have received a message from {name}.\n
+        # With email: {email}\n
+        # Message: {message}\n'''
+        body = generate_email_html(email, name, message)
 
-        send_mail(
+        # send_mail(
+        #     subject,
+        #     body,
+        #     'settings.EMAIL_HOST_USER',
+        #     [recipient],
+        #     fail_silently=False,
+        # )
+        email_message = EmailMessage(
             subject,
             body,
             'settings.EMAIL_HOST_USER',
-            [recipient],
-            fail_silently=False,
+            [recipient]
         )
+        email_message.content_subtype = "html"  # Set the content type to HTML
+
+        email_message.send(fail_silently=False)
         # print(redirect_url)
         return redirect(redirect_url)
         # else:
