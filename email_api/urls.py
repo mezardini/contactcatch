fe364dbcfc22f5a226d6ffa22c14c9a839af1679
mezeiser <mezardini@gmail.com>
2024-05-24 15:43:12 +0100

    added a new endpoint

--- email_api/urls.py
+++ email_api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import SendEmail
+from .views import SendEmail, SendEmailFF
 urlpatterns = [
-    path('sendemail/', SendEmail.as_view(), name='SE')
+    path('sendemail/', SendEmail.as_view(), name='SE'),
+    path('sendemailFF/', SendEmailFF.as_view(), name='SEFF')
 ]
--- email_api/views.py
+++ email_api/views.py
@@ -54,7 +54,7 @@ class SendEmail(generics.CreateAPIView):
         message = request.data.get('message')
 
         subject = f'Message from {name} with email of: {email}'
-        body = f"Hello Mezard,\n\nYou have received a project review request for {project_name}.\nProject URL: {project_url}\n\nMessage: {message}\n\nBest regards"
+        body = f"Hello Mezard,\n\nYou have received a project review request for {project_name}.\nProject URL: {project_url}\n\nMessage: {message}\n"
 
         send_mail(
             subject,
@@ -64,3 +64,28 @@ class SendEmail(generics.CreateAPIView):
             fail_silently=False,
         )
         return redirect('https://feedbackflow.netlify.app/')
+
+
+class SendEmailFF(generics.CreateAPIView):
+    def post(self, request):
+
+        redirect_url = request.build_absolute_uri()
+        email = request.data.get('email')
+        name = request.data.get('name')
+        message = request.data.get('message')
+        recipient = request.data.get('recipient')
+
+        subject = f'Message from {name}'
+        body = f'''Hello Mezard,\n\n
+        You have received a message from {name}.\n\n
+        With email: {email}\n\n
+        Message: {message}\n'''
+
+        send_mail(
+            subject,
+            body,
+            'settings.EMAIL_HOST_USER',
+            [recipient],
+            fail_silently=False,
+        )
+        return redirect(redirect_url)
